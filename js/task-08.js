ed814d6e40b7d4cdfaa5db0070bbefd1cdda4029
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-05-12 17:15:02 +0300

    change 8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,20 +1,19 @@
-const formEl = document.querySelector(".login-form");
-formEl.addEventListener("submit", sendForm);
+const mainForm = document.querySelector(".login-form"); // добираюсь до форми
 
-function sendForm(event) {
-  event.preventDefault();
-
-  const emailInput = formEl.elements.email;
-  const passwordInput = formEl.elements.password;
-
-  if (emailInput.value.trim() === "" || passwordInput.value.trim() === "") {
-    alert("Please fill in all fields of the form.");
+mainForm.addEventListener("submit", (event) =>{ // даю івент дормі при сабміт
+  event.preventDefault(); // скидую стандартні настройки (щоб не перезавантажувалась сторінка при відправці) 
+  const emailInput = mainForm.elements.email; // добираюсь до інпута з емейл
+  const passwordInput = mainForm.elements.password; // добираюсь до інпута з паролем
+  
+  if (emailInput.value.trim() === "" || passwordInput.value.trim() === "") { // даю умову якщо інпути пусті тоді...
+    alert("Please fill in all fields of the form."); // виведе вікно з написом
     return;
   }
-
-  const dataForm = {};
-  dataForm[emailInput.name] = emailInput.value.trim();
+  
+  const dataForm = {}; // 
+  dataForm[emailInput.name] = emailInput.value.trim(); 
   dataForm[passwordInput.name] = passwordInput.value.trim();
   console.log(dataForm);
-  formEl.reset();
-}
+  mainForm.reset();
+
+});
